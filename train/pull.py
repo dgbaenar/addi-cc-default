fd643a02b503aebae03404e3c356ada5a8b123c1
Daniel Baena <dgbaear@gmail.com>
2021-10-25 13:24:18 -0500

    Change files to experiment

--- train/pull.py
+++ train/pull.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00350/default%20of%20credit%20card%20clients.xls"
 
+
 df = pd.read_excel(url, header=1)
 
 df.to_csv("./data/raw.csv", index=False)
